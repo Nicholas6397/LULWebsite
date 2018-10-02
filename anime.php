6656a0ac7a85ed7cf3f8504950ebb2e732229a48
Nicholas6397 <38874638+Nicholas6397@users.noreply.github.com>
2018-10-01 23:07:25 -0500

    New Homepage

---
+++ anime.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Славянское Аниме</title>
+		<link rel="stylesheet" type="text/css" href="./style.css">
+	</head>
+
+	<body>
+		<div class="wrapper"></div>
+		<img class="tigerLogo" src="https://i.pinimg.com/originals/06/fb/09/06fb09b432edcd014b5a5b201847af9d.png">
+		<form action="./process.php" method="post">
+			Enter your favourite anime: <input name="name" type="text">
+			<input type="submit" value="Submit for judgement">
+		</form>
+		<div>
+			<img class="taiga" src="https://png2.kisspng.com/20180422/zke/kisspng-anime-toradora-taiga-aisaka-lelouch-lamperouge-ma-shopping-online-5adc1663428ff2.0089724415243730912727.png">
+		</div>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -7,13 +7,16 @@
 
 	<body>
 		<div class="wrapper"></div>
-		<img class="tigerLogo" src="https://i.pinimg.com/originals/06/fb/09/06fb09b432edcd014b5a5b201847af9d.png">
-		<form action="./process.php" method="post">
-			Enter your favourite anime: <input name="name" type="text">
-			<input type="submit" value="Submit for judgement">
-		</form>
-		<div>
-			<img class="taiga" src="https://png2.kisspng.com/20180422/zke/kisspng-anime-toradora-taiga-aisaka-lelouch-lamperouge-ma-shopping-online-5adc1663428ff2.0089724415243730912727.png">
+		<div class="content">
+			<img class="tigerLogo" src="https://i.pinimg.com/originals/06/fb/09/06fb09b432edcd014b5a5b201847af9d.png"><br>
+			<?php
+				$secret = mt_rand(0, 1);
+				if ($secret == 1){
+					echo "<a style='color: blue' href='./anime.php'>Enter if you dare</a>";
+				} else{
+					echo "Nothing to see here, Камрад"
+				}
+			?>
 		</div>
 	</body>
 </html>
